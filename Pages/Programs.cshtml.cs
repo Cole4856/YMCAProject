94663191572510b7924e38a60dfb34fe4ab95137
kyliet12 <106704044+kyliet12@users.noreply.github.com>
2024-12-04 15:48:44 -0600

    Add family registration

--- Pages/Programs.cshtml.cs
+++ Pages/Programs.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MySql.Data.MySqlClient;
+using YMCAProject.Models;
 
 namespace YMCAProject.Pages;
 
@@ -22,64 +23,68 @@ public class ProgramsModel : PageModel
     [BindProperty(SupportsGet = true)]
     public int Status { get; set; } = 1;
 
+    public List<Member> FamilyMembers { get; set; } = new();
+
     private readonly IConfiguration _configuration;
     public ProgramsModel(IConfiguration configuration)
     {
         _configuration = configuration;
     }
 
-    public List<Models.Programs> programList {get; set;} = [];
+    public List<Programs> programList {get; set;} = [];
 
     // Register button
-    public IActionResult OnPostRegisterClass(string className, int programId, DateTime startDate, DateTime startTime, string days)
+    public IActionResult OnPostRegisterClass(int memberId, string className, int programId, DateTime startDate, DateTime startTime, string days)
     {
+        LoadFamilyMembers();
+        Member mem = FamilyMembers.FirstOrDefault(m => m.MemberId == memberId);
         try{
-                string connectionString = _configuration.GetConnectionString("Default");
+            string connectionString = _configuration.GetConnectionString("Default");
 
-                using (MySqlConnection connection = new MySqlConnection(connectionString)){
-                    connection.Open();
+            using (MySqlConnection connection = new MySqlConnection(connectionString)){
+                connection.Open();
 
-                    // check if already registered
-                    string sql = "SELECT Count(*) FROM Member_Programs " +
-                        "WHERE MemberId = @MemberId AND ProgramID = @ProgramId ";
+                // check if already registered
+                string sql = "SELECT Count(*) FROM Member_Programs " +
+                    "WHERE MemberId = @MemberId AND ProgramID = @ProgramId ";
 
-                    using (MySqlCommand command = new MySqlCommand(sql, connection)){
-                        command.Parameters.AddWithValue("@MemberId", int.Parse(User.FindFirst("UserId")?.Value));            //change to memberId!!!!!!!!
-                        command.Parameters.AddWithValue("@ProgramId", programId); 
+                using (MySqlCommand command = new MySqlCommand(sql, connection)){
+                    command.Parameters.AddWithValue("@MemberId", mem.MemberId);
+                    command.Parameters.AddWithValue("@ProgramId", programId); 
 
-                        using (MySqlDataReader reader = command.ExecuteReader()) {
-                            reader.Read();
-                            int count = reader.GetInt32(0);
+                    using (MySqlDataReader reader = command.ExecuteReader()) {
+                        reader.Read();
+                        int count = reader.GetInt32(0);
 
-                            if (count > 0){
-                                // Show a failure message 
-                                TempData["RegisterMessage"] = $"Error: {User.Identity.Name} is already registered for {className}";
-                                TempData["MessageType"] = "error";
-                                
-                                // Redirect to the same page to show the message
-                                return RedirectToPage();
-                            }
-
+                        if (count > 0){
+                            // Show a failure message 
+                            TempData["RegisterMessage"] = $"Error: {mem.FirstName} {mem.LastName} is already registered for {className}";
+                            TempData["MessageType"] = "error";
+                            
+                            // Redirect to the same page to show the message
+                            return RedirectToPage();
                         }
+
                     }
+                }
 
-                    // check for schedule conflicts
-                    sql = "SELECT p.program_id, p.class_name, p.days, p.start_date, p.end_date, p.start_time, p.end_time " +
-                            "FROM Programs p " +
-                            "JOIN Member_Programs mp on p.program_id = mp.ProgramId " +
-                            "WHERE mp.MemberId = @MemberId " +
-                                "AND (@startDate BETWEEN p.start_date AND p.end_date) " +
-                                "AND (@startTime BETWEEN TIME(p.start_time) AND TIME(p.end_time))";
+                // check for schedule conflicts
+                sql = "SELECT p.program_id, p.class_name, p.days, p.start_date, p.end_date, p.start_time, p.end_time " +
+                        "FROM Programs p " +
+                        "JOIN Member_Programs mp on p.program_id = mp.ProgramId " +
+                        "WHERE mp.MemberId = @MemberId " +
+                            "AND (@startDate BETWEEN p.start_date AND p.end_date) " +
+                            "AND (TIME(@startTime) BETWEEN TIME(p.start_time) AND TIME(p.end_time))";
 
-                    string[] dayArray = days.Split(',');
+                string[] dayArray = days.Split(',');
 
-                    using (MySqlCommand command = new MySqlCommand(sql, connection)){
-                        command.Parameters.AddWithValue("@MemberId", int.Parse(User.FindFirst("UserId")?.Value));
-                        command.Parameters.AddWithValue("@startDate", startDate);         
-                        command.Parameters.AddWithValue("@startTime", startTime); 
+                using (MySqlCommand command = new MySqlCommand(sql, connection)){
+                    command.Parameters.AddWithValue("@MemberId", mem.MemberId);
+                    command.Parameters.AddWithValue("@startDate", startDate);         
+                    command.Parameters.AddWithValue("@startTime", startTime); 
 
-                        using (MySqlDataReader reader = command.ExecuteReader()) {
-                            reader.Read();
+                    using (MySqlDataReader reader = command.ExecuteReader()) {
+                        while (reader.Read()){
                             string programDays = reader.GetString(2);
 
                             foreach (var day in dayArray)
@@ -87,7 +92,7 @@ public class ProgramsModel : PageModel
                                 int index = programDays.IndexOf(day);
                                 if (index != -1){
                                     // Show a failure message 
-                                    TempData["RegisterMessage"] = $"Error: unable to register for {className} because it overlaps with {reader.GetString(1)}";
+                                    TempData["RegisterMessage"] = $"Error: {mem.FirstName} {mem.LastName} is unable to register for {className} because it overlaps with {reader.GetString(1)}";
                                     TempData["MessageType"] = "error";
                                     
                                     // Redirect to the same page to show the message
@@ -97,28 +102,29 @@ public class ProgramsModel : PageModel
                             }
                         }
                     }
-
-                    // register
-                    sql = "Insert INTO Member_Programs " +
-                        "(MemberId, ProgramId) VALUES " +
-                        "(@MemberId, @ProgramId)";
-
-                    using (MySqlCommand command = new MySqlCommand(sql, connection)){
-                        command.Parameters.AddWithValue("@MemberId", int.Parse(User.FindFirst("UserId")?.Value));  //change to memberId!!!!!!!!
-                        command.Parameters.AddWithValue("@ProgramId", programId); 
-                        
-                        command.ExecuteNonQuery();
-                    }
                 }
 
+                // register
+                sql = "Insert INTO Member_Programs " +
+                    "(MemberId, ProgramId) VALUES " +
+                    "(@MemberId, @ProgramId)";
+
+                using (MySqlCommand command = new MySqlCommand(sql, connection)){
+                    command.Parameters.AddWithValue("@MemberId", mem.MemberId); 
+                    command.Parameters.AddWithValue("@ProgramId", programId); 
+                    
+                    command.ExecuteNonQuery();
+                }
             }
-            catch(Exception ex){
-                Console.WriteLine("We have an error: " + ex.Message);
-            }
+
+        }
+        catch(Exception ex){
+            Console.WriteLine("We have a sql error: " + ex.Message);
+        }
 
 
         // Show a success message 
-        TempData["RegisterMessage"] = $"{User.Identity.Name} successfully registered for {className}!";
+        TempData["RegisterMessage"] = $"{mem.FirstName} {mem.LastName} is successfully registered for {className}!";
         TempData["MessageType"] = "success";
         
         // Redirect to the same page to show the message
@@ -160,12 +166,13 @@ public class ProgramsModel : PageModel
     }
     public void OnGet()
     {
+        LoadFamilyMembers();
         try{
             string connectionString = _configuration.GetConnectionString("Default");
 
             using (MySqlConnection connection = new MySqlConnection(connectionString)){
                 connection.Open();
-
+                // Get Programs list
                 string sql = "SELECT p.*, (p.capacity - COALESCE(m.registered_count, 0)) AS `spotsLeft`" +
                     "FROM ymca.Programs p LEFT JOIN ( " +
                         "SELECT ProgramId, COUNT(MemberId) AS registered_count FROM Member_Programs GROUP BY ProgramId" +
@@ -176,7 +183,7 @@ public class ProgramsModel : PageModel
 
                     using (MySqlDataReader reader = command.ExecuteReader()) {
                         while (reader.Read()){
-                            Models.Programs classInfo = new Models.Programs();
+                            Programs classInfo = new Programs();
 
                             classInfo.ProgramId = reader.GetInt32(0);
                             classInfo.ClassName = reader.GetString(1);
@@ -225,6 +232,45 @@ public class ProgramsModel : PageModel
         }
         catch(Exception ex){
             Console.WriteLine("We have an error: " + ex.Message);
+        }
+    }
+
+    private void LoadFamilyMembers(){
+        try{
+            string connectionString = _configuration.GetConnectionString("Default");
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString)){
+                connection.Open();
+                // Get Family Members
+                string sql = "SELECT * FROM Members " +
+                    "WHERE FamilyID = (SELECT FamilyID FROM Members WHERE MemberID = @id);";
+
+                using (MySqlCommand command = new MySqlCommand(sql, connection)){
+                    command.Parameters.AddWithValue("@id", int.Parse(User.FindFirst("UserId")?.Value));
+
+                    using (MySqlDataReader reader = command.ExecuteReader()) {
+                        while (reader.Read()){
+                            Member newMem = new Member();
+
+                            newMem.MemberId = reader.GetInt32(0);
+                            newMem.FirstName = reader.GetString(1);
+                            newMem.LastName = reader.GetString(2);
+                            newMem.Email = reader.GetString(3);
+                            newMem.PasswordHash = reader.GetString(4);
+                            newMem.IsActive = reader.GetBoolean(5);
+                            newMem.IsMember = reader.GetBoolean(6);
+                            newMem.FamilyId = reader.GetInt32(7);
+
+                            if (newMem.IsActive){
+                                FamilyMembers.Add(newMem);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        catch(Exception ex){
+            Console.WriteLine("We have an error when loading family members: " + ex.Message);
         }
     }
 }
