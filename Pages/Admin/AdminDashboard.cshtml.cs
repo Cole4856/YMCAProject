c931b15a999fbe0c64f9ea4881c6b82febf8bde0
Cole Hansen <hansen9168@uwlax.edu>
2024-12-08 23:55:43 -0600

    generate report

--- Pages/Admin/AdminDashboard.cshtml.cs
+++ Pages/Admin/AdminDashboard.cshtml.cs
@@ -2,6 +2,7 @@ using System.Dynamic;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MySql.Data.MySqlClient;
 using Microsoft.AspNetCore.Mvc;
+using System.Text;
 
 namespace YMCAProject.Pages.Admin
 {
@@ -15,6 +16,92 @@ public class AdminDashboard : PageModel
     }
 
     public List<Models.Member> memberList {get; set;} = [];
+    public List<Models.MemberProgram> memberPrograms {get; set;} = [];
+
+    public IActionResult OnPostGenerateReport(DateTime start, DateTime end){
+
+        //set end to 30th, 31st, 29th, or 28th of month
+        DateTime endDate = new DateTime(end.Year, end.Month, DateTime.DaysInMonth(end.Year, end.Month));
+  
+        try
+        {
+            string connectionString = _configuration.GetConnectionString("Default");
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString)){
+                connection.Open();
+
+                string sql = "SELECT m.MemberId, " +
+                             "CONCAT(m.FirstName, ' ', m.LastNAme) AS member_name, " +
+                             "p.program_id, p.class_name, p.start_date, p.end_date " +
+                             "FROM Members m " + 
+                             "JOIN Member_Programs mp ON m.MemberId = mp.MemberId " +
+                             "JOIN Programs p ON mp.ProgramId = p.program_id " +
+                             "WHERE p.start_date >= @Start " +
+                             "AND p.end_date <= @End "+
+                             "ORDER BY m.MemberId, p.start_date;";
+                using (MySqlCommand command = new MySqlCommand(sql, connection)){
+                    command.Parameters.AddWithValue("@Start", start);
+                    command.Parameters.AddWithValue("@End", endDate); 
+
+                    using (MySqlDataReader reader = command.ExecuteReader()) {
+                        while(reader.Read()){
+                            var mp = new Models.MemberProgram();
+
+                            mp.MemberId = reader.GetInt32(0);
+                            mp.MemberName = reader.GetString(1);
+                            mp.ProgramId = reader.GetInt32(2);
+                            mp.ClassName = reader.GetString(3);
+                            mp.StartDate = reader.GetDateTime(4);
+                            mp.EndDate = reader.GetDateTime(5);
+
+                            memberPrograms.Add(mp);
+
+                        }
+                    }
+                }
+
+
+            }
+
+            Console.WriteLine(memberPrograms.Count);
+            var csvBuilder = new StringBuilder();
+
+            // Write the CSV headers
+             csvBuilder.AppendLine("MemberId,Name,ProgramId,ClassName,StartDate,EndDate");
+
+            // Write the CSV rows
+            foreach (var item in memberPrograms)
+            {
+             csvBuilder.AppendLine($"{item.MemberId}," +
+                                  $"{item.MemberName}," +
+                                  $"{item.ProgramId}," +
+                                  $"{item.ClassName}," +
+                                  $"{item.StartDate:yyyy-MM-dd}," +
+                                  $"{item.EndDate:yyyy-MM-dd}");
+            }
+
+            // Convert the StringBuilder content to a byte array
+            byte[] csvBytes = Encoding.UTF8.GetBytes(csvBuilder.ToString());
+
+            // Return the CSV file as a downloadable response
+
+            
+            return File(csvBytes, "text/csv", "MemberPrograms.csv");
+
+        }
+        
+        catch(Exception ex)
+        {
+            Console.WriteLine("We have an error generating report: " + ex.Message);
+        }
+          
+        // Show a success message 
+        TempData["RegisterMessage"] = $"Report Generated, please check your downloads";
+        TempData["MessageType"] = "success";
+        
+        // Redirect to the same page to show the message
+        return RedirectToPage();
+    }
 
     // Remove User button
     public IActionResult OnPostDeleteUser(int memberId, string fname, string lname){
